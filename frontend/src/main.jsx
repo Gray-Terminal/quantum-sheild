7dfa670e674b60e76e36e2e8e59db22d2251ce61
Varchas HV <hvvarchas@gmail.com>
2025-11-02 13:30:42 +0530

    changed main.jsx again

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,4 +6,4 @@ import './App.css';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<QuantumShieldFileEncryptor />);
+root.render(<QuantumShield />);
