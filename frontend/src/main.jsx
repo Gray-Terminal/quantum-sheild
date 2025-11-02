16f3311b8d138580204745dd258077fe7f0d43a4
Varchas HV <hvvarchas@gmail.com>
2025-11-02 13:27:38 +0530

    changed main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 ﻿// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
-import QuantumShieldFileEncryptor from "./App.jsx";
+import QuantumShield from "./App.jsx";
 import './App.css';
 
 const container = document.getElementById("root");
