5cbe87837702f27e86ec616d4c4c87608a46fa26
Varchas HV <hvvarchas@gmail.com>
2025-11-02 13:34:58 +0530

    changed config

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,2 @@
-// src/config.js
-const config = {
-  API_BASE_URL: process.env.REACT_APP_API_URL || "http://localhost:8000"
-};
-export default config;
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
